d820dc0429f5bab708b30f7f2b791626d304dd24
AtomicNicos <atomicnicos@gmail.com>
2021-05-02 10:26:39 +0200

    add favicon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have a parameter false`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.container).toEqual(false);
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
